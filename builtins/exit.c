5f159c79e828f8443d77ff1f1d404a578711a1f6
Smail Oujaoura <email.oujaoura@gmail.com>
2025-03-04 11:55:42 +0000

    fix exit

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,47 +6,36 @@
 /*   By: soujaour <soujaour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 20:50:59 by bkolani           #+#    #+#             */
-/*   Updated: 2025/03/03 16:27:35 by soujaour         ###   ########.fr       */
+/*   Updated: 2025/03/04 11:53:31 by soujaour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	check_exit_status(char **argv, int *status)
+void	builtin_exit(char **argv, int *status)
 {
-	int	i;
-	int	n;
 	int	error;
+	int	num;
 
-	i = 0;
 	error = 0;
-	n = ft_atoi(argv[1], &error);
-	while (argv[i])
+	if (!argv[1])
+		exit(*status = 0);
+	else if (argv[1])
 	{
-		error = 0;
-		n = ft_atoi(argv[1], &error);
+		num = ft_atoi(argv[1], &error);
 		if (error)
 		{
 			printf("minishell: exit: %s: numeric argument required\n", argv[1]);
 			*status = 2;
 			exit(2);
 		}
-		i++;
+		else if (argv[2])
+		{
+			write(2, "minishell: exit: too many arguments\n", 36);
+			*status = 1;
+			return ;
+		}
+		else
+			exit(num % 256);
 	}
-	if (i >= 1)
-	{
-		write(2, "minishell: exit: too many arguments\n", 36);
-		*status = 1;
-		return ;
-	}
-}
-
-void	builtin_exit(char **argv, int *status)
-{
-	if (!argv[1])
-	{
-		*status = 0;
-		exit(EXIT_SUCCESS);
-	}
-	check_exit_status(argv, status);
 }
