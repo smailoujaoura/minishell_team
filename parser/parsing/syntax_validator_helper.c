8543e7d5ef044dca23605fa0f5553ffb380dd915
Smail Oujaoura <email.oujaoura@gmail.com>
2025-03-20 18:29:24 +0000

    fix syntax error reporting

--- parser/parsing/syntax_validator_helper.c
+++ parser/parsing/syntax_validator_helper.c
@@ -6,7 +6,7 @@
 /*   By: soujaour <soujaour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 12:41:27 by soujaour          #+#    #+#             */
-/*   Updated: 2025/03/20 17:58:10 by soujaour         ###   ########.fr       */
+/*   Updated: 2025/03/20 18:28:43 by soujaour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ int	check_pipe(t_chain *prev, t_chain *current, t_chain *next)
 {
 	if (next == NULL)
 		return (0);
-	if (prev == NULL || (prev->type != WORD && prev->type != R_PAREN))
+	if (prev == NULL || (prev->type != WORD && prev->type != R_PAREN
+		&& !is_redir(prev, IN + OR + OUT)))
 	{
 		write_four_strings(SYNTAX, " `", current->content, "'\n");
 		return (1);
@@ -25,15 +26,9 @@ int	check_pipe(t_chain *prev, t_chain *current, t_chain *next)
 		|| next->type == R_PAREN)
 	{
 		if (!next)
-		{
-			write(2, SYNTAX, ft_strlen(SYNTAX));
-			write(2, " 'newline'\n", 11);
-		}
+			write_four_strings(SYNTAX, " 'newline'\n", NULL, NULL);
 		else if (!prev)
-		{
-			write(2, SYNTAX, ft_strlen(SYNTAX));
-			write(2, " `|'\n", 5);
-		}
+			write_four_strings(SYNTAX, " `|'\n", NULL, NULL);
 		else
 			write_four_strings(SYNTAX, " `", next->content, "'\n");
 		return (1);
@@ -45,7 +40,8 @@ int	check_logicals(t_chain *prev, t_chain *current, t_chain *next)
 {
 	if (next == NULL)
 		return (0);
-	if (prev == NULL || (prev->type != WORD && prev->type != R_PAREN))
+	if (prev == NULL || (prev->type != WORD && prev->type != R_PAREN
+		&& !is_redir(prev, IN + OR + OUT)))
 	{
 		write_four_strings(SYNTAX, " `", current->content, "'\n");
 		return (1);
